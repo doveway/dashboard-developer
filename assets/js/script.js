d22add6fc72cec6a038996409a6d9f622bc7e0db
anu <oderinloanuoluwapo@gmail.com>
2023-11-30 10:40:42 +0100

    feat: implemented the login and first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("close").addEventListener("click", function(){
+    document.getElementById("mobile_navigation_container").style.display = "none";
+});
+
+document.getElementById("toggle_menu").addEventListener("click", function(){
+    document.getElementById("mobile_navigation_container").style.display = "block";
+});
+
